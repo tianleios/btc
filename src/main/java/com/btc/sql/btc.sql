d691984edb45a58b16e59118122b0792dec64d8f
tinaleios <tianleios@163.com>
2018-02-06 20:39:49 +0800

    sql

--- src/main/java/com/btc/sql/btc.sql
+++ src/main/java/com/btc/sql/btc.sql
@@ -1,5 +1,26 @@
+/*
+ Navicat Premium Data Transfer
 
-##地址
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost
+ Source Database       : btc
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : utf-8
+
+ Date: 02/06/2018 20:35:13 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `btc_address`
+-- ----------------------------
+DROP TABLE IF EXISTS `btc_address`;
 CREATE TABLE `btc_address` (
   `address` char(36) NOT NULL COMMENT '地址',
   `privatekey` varchar(64) NOT NULL,
@@ -7,58 +28,61 @@ CREATE TABLE `btc_address` (
   PRIMARY KEY (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-##归集utxo
+-- ----------------------------
+--  Table structure for `btc_default_utxo`
+-- ----------------------------
+DROP TABLE IF EXISTS `btc_default_utxo`;
 CREATE TABLE `btc_default_utxo` (
-  `txid` CHAR(64) NOT NULL
-  COMMENT '交易id',
-  `vout` INT      NOT NULL,
-  `count` DECIMAL(64,0) NOT NULL,
-  `script_pub_key` TEXT NOT NULL,
-  `address` CHAR(34) NOT NULL ,
-  `sync_time` TIMESTAMP NOT NULL ,
-  `block_height` INT NOT NULL ,
-  `status` VARCHAR(4) NOT NULL ,
-  `spend_txid` CHAR(64) NOT NULL,
-  `collection_time` DATETIME NULL COMMENT '归集时间',
-
-  PRIMARY KEY (`txid`, `vout`),
-  KEY `address`(address),
-  KEY `status`(status)
-
+  `txid` char(64) NOT NULL COMMENT '交易id',
+  `vout` int(11) NOT NULL,
+  `count` decimal(64,0) NOT NULL,
+  `script_pub_key` text NOT NULL,
+  `address` char(34) NOT NULL,
+  `sync_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `block_height` int(11) NOT NULL,
+  `status` varchar(4) NOT NULL,
+  `spend_txid` char(64) DEFAULT NULL,
+  `collection_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`txid`,`vout`),
+  KEY `address` (`address`),
+  KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-##提现utxo
-CREATE TABLE `btc_withdraw_utxo` (
+-- ----------------------------
+--  Table structure for `btc_withdraw`
+-- ----------------------------
 
-  `txid` CHAR(64) NOT NULL
-  COMMENT '交易id',
-  `vout` INT      NOT NULL,
-  `count` DECIMAL(64,0) NOT NULL,
-  `script_pub_key` TEXT NOT NULL,
-  `address` VARCHAR(40) NOT NULL ,
-  `sync_time` TIMESTAMP NOT NULL ,
-  `block_height` INT NOT NULL ,
-  `status` VARCHAR(4) NOT NULL ,
-  `withdraw_txid` CHAR(64) NOT NULL,
-  PRIMARY KEY (`txid`, `vout`),
-  KEY `address`(address),
-  KEY `status`(status)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-##提现申请表
+DROP TABLE IF EXISTS `btc_withdraw`;
 CREATE TABLE `btc_withdraw` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `to_address` CHAR(34) NOT NULL ,
-  `count` DECIMAL(64,0) NOT NULL ,
-  `apply_datetime` DATETIME NOT NULL,
-  `update_datetime` DATETIME NOT NULL,
-  `status` VARCHAR(4) NOT NULL ,
-  `withdraw_txid` CHAR(64) NOT NULL DEFAULT '',
-
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `to_address` char(34) NOT NULL,
+  `count` decimal(64,0) NOT NULL,
+  `apply_datetime` datetime NOT NULL,
+  `update_datetime` datetime NOT NULL,
+  `status` varchar(4) NOT NULL,
+  `withdraw_txid` char(64) DEFAULT '',
   PRIMARY KEY (`id`),
-  KEY `withdraw_txid_key`(`withdraw_txid`),
-  KEY `status_key`(`status`)
+  KEY `withdraw_txid_key` (`withdraw_txid`),
+  KEY `status_key` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-
+-- ----------------------------
+--  Table structure for `btc_withdraw_utxo`
+-- ----------------------------
+DROP TABLE IF EXISTS `btc_withdraw_utxo`;
+CREATE TABLE `btc_withdraw_utxo` (
+  `txid` char(64) NOT NULL COMMENT '交易id',
+  `vout` int(11) NOT NULL,
+  `count` decimal(64,0) NOT NULL,
+  `script_pub_key` text NOT NULL,
+  `address` char(34) NOT NULL,
+  `sync_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `block_height` int(11) NOT NULL,
+  `status` varchar(4) NOT NULL,
+  `withdraw_txid` char(64) DEFAULT NULL,
+  PRIMARY KEY (`txid`,`vout`),
+  KEY `address` (`address`),
+  KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
